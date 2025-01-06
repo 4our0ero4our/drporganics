574ced02aa7e11350f2c53ddfd42fd2b4f389a2c
Muhammed <oparemimuhammedkolapo@gmail.com>
2025-01-06 19:52:55 +0100

    Initial commit - Hero and About Section Only

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import AboutUsImg4 from '../Assets/Images/AboutUsImg4.jpeg';
+import AboutUsImg2 from '../Assets/Images/AboutUsImg2.jpg';
+import FreshLeaf from '../Assets/Images/A fresh Leaf Edited.jpg';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const About = () => {
+    useEffect(() => {
+        AOS.init({ duration: 3000 });
+    }, []);
+
+    return (
+        <section className="about" id="about">
+            <div className='what-we-do' data-aos="fade-right">
+                <h1>What We do at <br />Dr. P Organic Consults</h1>
+                <p>
+                    We provide effective treatment for conditions such as Chronic ulcers, Kidney Diseases, Fibroid, Hypertension, Diabetes, Parkinson's Disease, HIV, and Sickle Cell Anemia, among others. With Allah’s Mercy, we guarantee positive results tailored to your needs.
+                </p>
+                <img src={AboutUsImg4} alt="Fresh leaves" />
+            </div>
+            <img className='about-fresh-leaf' data-aos="fade-up" src={FreshLeaf} alt="A fresh Leaf" />
+            <div className='about-us' data-aos="fade-left">
+                <div>
+                    <h2>About Us</h2>
+                    <p>
+                        We are a Nigerian company dedicated to manufacturing herbal medicines for various human health challenges. Our remedies are natural, effective, and tailored to ensure the well-being of our clients. From chronic illnesses to general health concerns, we provide a trusted pathway to healing.
+                    </p>
+                </div>
+                <img src={AboutUsImg2} alt="Herbs beign grown in vase" />
+            </div>
+        </section>
+    )
+}
+
+export default About;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,91 @@
+// import HerbPlants1 from '../Assets/Images/HerbPlants1.jpg';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import { FaLeaf } from "react-icons/fa";
+import { TiLeaf } from 'react-icons/ti';
+import { useState, useEffect } from 'react';
+import { MdCancel } from 'react-icons/md';
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io5";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const imageRoutes = ['HerbPlants1.jpg', 'HerbPlants2.jpg', 'HerbPlants3.jpg'];
+
+const Hero = () => {
+    const [openNav, setOpenNav] = useState(false);
+    const [imagePosition, setImagePosition] = useState(1);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setImagePosition((prevPosition) => {
+                if (prevPosition === 3) return 1;
+                return prevPosition + 1;
+            });
+        }, 5000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    useEffect(() => {
+        AOS.init({ duration: 2000 });
+    }, []);
+
+    return (
+        <section id='about' style={{ backgroundImage: `url(${require(`../Assets/Images/${imageRoutes[imagePosition - 1]}`)})`, transition: 'all 1s ease' }} className="hero">
+            <div className='hero-overlay'></div>
+            <nav className='desktop-nav' data-aos="fade-down">
+                <h1 className='logo organics-text'>Dr P. <span>Organics</span> <FaLeaf style={{ color: "rgb(0, 255, 0)" }} /></h1>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#consultus">Consult Us</a></li>
+                    <li><a href="#estimonials">Testimonies</a></li>
+                </ul>
+            </nav>
+            <nav className='mobile-nav' data-aos="fade-down">
+                {openNav ? '' : <h1 className='logo organics-text'>Dr P. <span>Organics</span> <FaLeaf style={{ color: "rgb(0, 255, 0)" }} /></h1>}
+                {openNav ? '' : <TiLeaf className='swing' onClick={() => { setOpenNav(true) }} />}
+                {openNav ?
+                    <ul>
+                        <MdCancel className='cancel-btn' onClick={() => setOpenNav(false)} />
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#consultus">Consult Us</a></li>
+                        <li><a href="#estimonials">Testimonies</a></li>
+                    </ul>
+                    :
+                    ''
+                }
+
+            </nav>
+            <div className='hero-socials' data-aos="fade-left" data-aos-duration="500">
+                <div className='hero-social-line-top'></div>
+                <Link><FaSquareInstagram /></Link>
+                <Link><FaFacebookF /></Link>
+                <Link><FaXTwitter /></Link>
+                <Link><IoLogoYoutube /></Link>
+                <div className='hero-social-line-bottom'></div>
+            </div>
+            <div className="hero-content" data-aos="fade-up">
+                <h1>Empowering Your Health <span className="organics-text"> Naturally</span></h1>
+                <p>
+                    Discover the healing power of our herbal medicines, made from 100%
+                    natural ingredients for a wide range of human and animal health challenges.
+                </p>
+                <a href="#consultus">
+                    <button>Consult us</button>
+                </a>
+            </div>
+            <div className='image-indicator'>
+                <div style={{ backgroundColor: imagePosition === 1 ? '#fff' : '#6e6e6e', width: imagePosition === 1 ? '15px' : '10px', height: imagePosition === 1 ? '15px' : '10px', margin: '0 auto', borderRadius: "50%", transition: 'all 1s ease' }}></div>
+                <div style={{ backgroundColor: imagePosition === 2 ? '#fff' : '#6e6e6e', width: imagePosition === 2 ? '15px' : '10px', height: imagePosition === 2 ? '15px' : '10px', margin: '0 auto', borderRadius: "50%", transition: 'all 1s ease' }}></div>
+                <div style={{ backgroundColor: imagePosition === 3 ? '#fff' : '#6e6e6e', width: imagePosition === 3 ? '15px' : '10px', height: imagePosition === 3 ? '15px' : '10px', margin: '0 auto', borderRadius: "50%", transition: 'all 1s ease' }}></div>
+            </div>
+        </section >
+    )
+}
+
+export default Hero;
